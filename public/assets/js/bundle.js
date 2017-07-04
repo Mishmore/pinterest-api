ddf80394b79378c1104fb6a1024c229faab11b67
Mishmore <michellemore28@gmail.com>
2017-07-04 17:36:58 -0500

    get url de board

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -8,7 +8,28 @@ const render = (root) => {
   root.append(wrapper);
 }
 
+const hola = 'ada';
+const board = {
+  boardData: null,
+  img: null,
+  title: null,
+  author: null,
+  pinId: null
+}
+
 $( _ => {
+  /*
+  $.get("https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AYneH0AVANBioiyRpgDkY8hLv8LpFM4lRp9RIINEIt-RlsA7PgAAAAA&fields=id%2Clink%2Cimage%2Cattribution%2Cboard%2Ccolor%2Ccounts%2Ccreated_at%2Cmedia%2Cmetadata%2Cnote%2Coriginal_link%2Curl", function( data ) {
+    board.boardData = data;
+    console.log(board.boardData);
+  });
+*/
+  $.get( "https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AYneH0AVANBioiyRpgDkY8hLv8LpFM4lRp9RIINEIt-RlsA7PgAAAAA&fields=id%2Clink%2Cimage%2Cattribution%2Cboard%2Ccolor%2Ccounts%2Ccreated_at%2Cmedia%2Cmetadata%2Cnote%2Coriginal_link%2Curl")
+  .done(function( data ) {
+    board.boardData = data;
+    console.log(board.boardData);
+  });
+
   const root = $('.root');
   render(root);
   console.log('renderizando');
@@ -37,5 +58,11 @@ const Navbar = (update) => {
 
   return navbar;
 }
+/*
+(ID de pin) data.id
+(Título) data.metadata.link.title
+(Autor) data.attribution.author_name
+(Nombre de Board) data.board.name
+*/
 
 },{}]},{},[1])
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,7 +7,28 @@ const render = (root) => {
   root.append(wrapper);
 }
 
+const hola = 'ada';
+const board = {
+  boardData: null,
+  img: null,
+  title: null,
+  author: null,
+  pinId: null
+}
+
 $( _ => {
+  /*
+  $.get("https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AYneH0AVANBioiyRpgDkY8hLv8LpFM4lRp9RIINEIt-RlsA7PgAAAAA&fields=id%2Clink%2Cimage%2Cattribution%2Cboard%2Ccolor%2Ccounts%2Ccreated_at%2Cmedia%2Cmetadata%2Cnote%2Coriginal_link%2Curl", function( data ) {
+    board.boardData = data;
+    console.log(board.boardData);
+  });
+*/
+  $.get( "https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AYneH0AVANBioiyRpgDkY8hLv8LpFM4lRp9RIINEIt-RlsA7PgAAAAA&fields=id%2Clink%2Cimage%2Cattribution%2Cboard%2Ccolor%2Ccounts%2Ccreated_at%2Cmedia%2Cmetadata%2Cnote%2Coriginal_link%2Curl")
+  .done(function( data ) {
+    board.boardData = data;
+    console.log(board.boardData);
+  });
+
   const root = $('.root');
   render(root);
   console.log('renderizando');
@@ -36,3 +57,9 @@ const Navbar = (update) => {
 
   return navbar;
 }
+/*
+(ID de pin) data.id
+(Título) data.metadata.link.title
+(Autor) data.attribution.author_name
+(Nombre de Board) data.board.name
+*/
