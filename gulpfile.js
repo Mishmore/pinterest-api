e05ece0931d77f6e8c703f24274f1527862cb8cb
Mishmore <michellemore28@gmail.com>
2017-07-04 11:16:37 -0500

    boilerplate listo

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,22 @@ var config = {
   source: './src/',
   dist: './public'
 };
+
 var paths = {
   assets: "/assets/",
   html: "**/*.html",
   sass: "scss/**/*.scss",
   mainSass: "scss/main.scss",
-  mainJS: "js/app.js"
+  mainJS: "js/app.js",
 };
+
 var sources = {
   assets: config.source + paths.assets,
   html: config.source + paths.html,
   sass: paths.assets + paths.sass,
   js: config.source + paths.js,
   rootSass: config.source + paths.assets + paths.mainSass,
-  rootJS: config.source + paths.assets + paths.mainJS
+  rootJS: config.source + paths.assets + paths.mainJS,
 };
 gulp.task('html', ()=>{
   gulp.src(sources.html).pipe(gulp.dest(config.dist));
