d69bd3e2f2abceea63e3e02b2e6616a7b1a7e277
Mishmore <michellemore28@gmail.com>
2017-07-04 19:09:39 -0500

    gulp serve si no gulp todo

--- src/assets/js/components/navbar.js
+++ src/assets/js/components/navbar.js
@@ -1,6 +1,6 @@
 'use strict';
 const Navbar = (update) => {
-  const parent = $('<div class="navbar-fixed green"></div>');
+  const parent = $('<div class="navbar-fixed"></div>');
   const navbar = $('<nav class="navbar white"></nav>');
   const logo = $('<a href="" class="navbar__logo"></a>');
   const search = $('<div class="navbar__search hide-on-small-only"></div>');
