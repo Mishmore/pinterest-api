ab0e3e8f8fe1c963eba73c2194f8a8d4824f3027
Mishmore <michellemore28@gmail.com>
2017-07-05 16:29:44 -0500

    lupa

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -32,24 +32,11 @@ const render = (root) => {
   root.append(wrapper);
 }
 
-const hola = 'ada';
 const board = {
   boardData: null
 }
 
 $( _ => {
-  /*
-  $.get("https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AYneH0AVANBioiyRpgDkY8hLv8LpFM4lRp9RIINEIt-RlsA7PgAAAAA&fields=id%2Clink%2Cimage%2Cattribution%2Cboard%2Ccolor%2Ccounts%2Ccreated_at%2Cmedia%2Cmetadata%2Cnote%2Coriginal_link%2Curl", function( data ) {
-    board.boardData = data;
-    console.log(board.boardData);
-  });
-*/
-/*
-(ID de pin) data.id
-(Título) data.metadata.link.title
-(Autor) data.attribution.author_name
-(Nombre de Board) data.board.name
-*/
   $.get( "https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AYneH0AVANBioiyRpgDkY8hLv8LpFM4lRp9RIINEIt-RlsA7PgAAAAA&fields=id%2Clink%2Cimage%2Cattribution%2Cboard%2Ccolor%2Ccounts%2Ccreated_at%2Cmedia%2Cmetadata%2Cnote%2Coriginal_link%2Curl")
   .done(function( response ) {
     board.boardData = response.data;
