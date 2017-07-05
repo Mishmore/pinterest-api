1210639bd19e3ac85e0af78b7e86ac1e4110b5a6
Erika Vidal <diann852@gmail.com>
2017-07-04 19:23:28 -0500

    iconos hover

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -14,15 +14,22 @@ $( _ => {
   console.log('renderizando');
 });
 
-
 const Board=(update)=>{
 
   const contenedor=$('<div class="container"></div>');
   const row       =$('<div class="row"></div>');
-  const board     =$('<div class="col m4"></div>');
+  const pin       =$('<div class="col m4"></div>');
+  const overlay   =$('<div class="overlay"></div>');
+  const divIcon   =$('<div class="icon-div btn white"></div>');
+  const up        =$('<span class="icon-out"></span>')
+  const btn       =$('<a class="waves-effect waves-light btn boton">Guardar</a>');
 
-  row.append(board);
+  divIcon.append(up);
+  overlay.append(divIcon);
+  overlay.append(btn);
+  pin.append(overlay);
+  row.append(pin);
   contenedor.append(row);
-  return contenedor;
 
+  return contenedor;
 }
