20b4098271d9f6f0aad210172e049c62a6dc4924
Misshel <jenmish094@gmail.com>
2017-07-04 19:17:29 -0500

    scroll y tags

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,7 +10,6 @@ const render = (root) => {
 $( _ => {
   const root = $('.root');
   render(root);
-  console.log('renderizando');
 });
 
 const tagsGenerator = ()=>{
@@ -18,10 +17,13 @@ const tagsGenerator = ()=>{
                 "Colores","Arquitectura","Corporativo","Frases", "Empresarial", "Español", "Comida", "Restaurante",
                 "Tienda", "Productos", "Wordpress", "Desing", "Template", "Flat", "2017", "Creativo"
                 ]
-  const contTag = $('<div class="row"></div>');
+
+  const contTag = $('<div class="row tagScroll"></div>');
   const coltag = $('<div class="col s12 scroll-tags"></div>');
   for (var i = 0; i < tags.length; i++) {
     const tag = $(`<div class="btn-large scroll-tags__tags">${tags[i]}</div>`);
+    tag.addClass(`scroll-tags-${[i]}`);
+
     coltag.append(tag);
   }
   contTag.append(coltag);
