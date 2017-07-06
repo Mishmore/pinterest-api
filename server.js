796d67cc9cfb96f5cf765ce1a7976aa2184022d3
Mishmore <michellemore28@gmail.com>
2017-07-06 09:23:06 -0500

    cambio al serve

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use('/', express.static('public'));
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(3000, () => {
-  console.log('Listening on 3000');
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+app.use('/', express.static('public'));
